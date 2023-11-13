bf53d2a88a0708594a268e340381a6a519b31e8f
Olivier Giniaux <oginiaux@smartadserver.com>
2023-11-13 11:48:12 +0100

    Bench exceptions time

---
+++ Tata/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace Tata;
+
+public static class Program
+{
+    public static void Main()
+    {
+        const int iterations = 100_000;
+
+        for (int p = 1; p <= Environment.ProcessorCount; p++)
+        {
+            Console.WriteLine($"Paralellism: {p}");
+            long totalTimeNs = 0;
+            ExecuteConcurrently(() =>
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                for (int i = 0; i < iterations; i++)
+                {
+                    try
+                    {
+                        throw new OverflowException();
+                    }
+                    catch(OverflowException) {}
+                }
+                sw.Stop();
+                Interlocked.Add(ref totalTimeNs, (long)sw.Elapsed.TotalNanoseconds);
+            }, p);
+            Console.WriteLine($"Time per exception thrown: {1d * totalTimeNs / iterations / p} ns");
+        }
+    }
+
+    private static void ExecuteConcurrently(Action action, int parallelism)
+    {
+        var threads = new Thread[parallelism];
+        for (int i = 0; i < parallelism; i++)
+        {
+            threads[i] = new Thread(_ => action());
+            threads[i].Start();
+        }
+
+        for (int i = 0; i < parallelism; i++)
+        {
+            threads[i].Join();
+        }
+    }
+}
